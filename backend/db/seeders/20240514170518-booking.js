fcc9f18158eb3fbe8a77b0e68de9e9c5bdb140e0
Mengxuan Liang <mengxuanliang007@gmail.com>
2024-05-18 10:56:29 -0400

    edit

--- backend/db/seeders/20240514170518-booking.js
+++ backend/db/seeders/20240514170518-booking.js
@@ -28,18 +28,52 @@ module.exports = {
         endDate: '2024-02-05'
       },
       {
-        spotId: 1,
+        spotId: 4,
         userId: 1,
         startDate: '2024-06-01',
         endDate: '2024-06-05'
       },
       {
-        spotId: 2,
+        spotId: 5,
         userId: 2,
         startDate: '2024-07-01',
         endDate: '2024-07-05'
       },{
-        spotId: 3,
+        spotId: 6,
+        userId: 3,
+        startDate: '2024-08-01',
+        endDate: '2024-08-05'
+      },
+      {
+        spotId: 7,
+        userId: 1,
+        startDate: '2024-01-01',
+        endDate: '2024-01-05'
+      },
+      {
+        spotId: 8,
+        userId: 2,
+        startDate: '2024-03-01',
+        endDate: '2024-03-05'
+      },{
+        spotId: 9,
+        userId: 3,
+        startDate: '2024-02-01',
+        endDate: '2024-02-05'
+      },
+      {
+        spotId: 10,
+        userId: 1,
+        startDate: '2024-06-01',
+        endDate: '2024-06-05'
+      },
+      {
+        spotId: 11,
+        userId: 2,
+        startDate: '2024-07-01',
+        endDate: '2024-07-05'
+      },{
+        spotId: 12,
         userId: 3,
         startDate: '2024-08-01',
         endDate: '2024-08-05'
--- backend/db/seeders/20240514212510-review.js
+++ backend/db/seeders/20240514212510-review.js
@@ -18,11 +18,6 @@ module.exports = {
       },
       {
         spotId: 1,
-        userId: 2,
-        review: 'even greater place to stay',
-        stars: 4
-      },{
-        spotId: 1,
         userId: 3,
         review: 'not a great place to stay',
         stars: 5
@@ -42,11 +37,6 @@ module.exports = {
         userId: 3,
         review: 'this palce is great',
         stars: 3
-      },{
-        spotId: 3,
-        userId:1,
-        review: 'i will come back again',
-        stars: 2
       },
       {
         spotId: 3,
@@ -55,6 +45,158 @@ module.exports = {
         stars: 2
       },{
         spotId: 3,
+        userId: 3,
+        review: 'this is the best place ever',
+        stars: 5
+      },
+      {
+        spotId: 4,
+        userId: 1,
+        review: 'this is a great place to stay',
+        stars: 1
+      },
+      {
+        spotId: 4,
+        userId: 2,
+        review: 'do not come here',
+        stars: 4
+      },{
+        spotId: 4,
+        userId: 3,
+        review: 'this palce is great',
+        stars: 3
+      },{
+        spotId: 5,
+        userId:1,
+        review: 'i will come back again',
+        stars: 2
+      },
+      {
+        spotId: 5,
+        userId: 2,
+        review: 'not a bad place',
+        stars: 2
+      },{
+        spotId: 5,
+        userId: 3,
+        review: 'this is the best place ever',
+        stars: 5
+      },
+      {
+        spotId: 6,
+        userId:1,
+        review: 'i will come back again',
+        stars: 2
+      },
+      {
+        spotId: 6,
+        userId: 2,
+        review: 'not a bad place',
+        stars: 2
+      },{
+        spotId: 6,
+        userId: 3,
+        review: 'this is the best place ever',
+        stars: 5
+      },
+      {
+        spotId: 7,
+        userId:1,
+        review: 'i will come back again',
+        stars: 2
+      },
+      {
+        spotId: 7,
+        userId: 2,
+        review: 'not a bad place',
+        stars: 2
+      },{
+        spotId: 7,
+        userId: 3,
+        review: 'this is the best place ever',
+        stars: 5
+      },
+      {
+        spotId: 8,
+        userId:1,
+        review: 'i will come back again',
+        stars: 2
+      },
+      {
+        spotId: 8,
+        userId: 2,
+        review: 'not a bad place',
+        stars: 2
+      },{
+        spotId: 8,
+        userId: 3,
+        review: 'this is the best place ever',
+        stars: 5
+      },
+      {
+        spotId: 9,
+        userId:1,
+        review: 'i will come back again',
+        stars: 2
+      },
+      {
+        spotId: 9,
+        userId: 2,
+        review: 'not a bad place',
+        stars: 2
+      },{
+        spotId: 9,
+        userId: 3,
+        review: 'this is the best place ever',
+        stars: 5
+      },
+      {
+        spotId: 10,
+        userId:1,
+        review: 'i will come back again',
+        stars: 2
+      },
+      {
+        spotId: 10,
+        userId: 2,
+        review: 'not a bad place',
+        stars: 2
+      },{
+        spotId: 10,
+        userId: 3,
+        review: 'this is the best place ever',
+        stars: 5
+      },
+      {
+        spotId: 11,
+        userId:1,
+        review: 'i will come back again',
+        stars: 2
+      },
+      {
+        spotId: 11,
+        userId: 2,
+        review: 'not a bad place',
+        stars: 2
+      },{
+        spotId:11,
+        userId: 3,
+        review: 'this is the best place ever',
+        stars: 5
+      },
+      {
+        spotId: 12,
+        userId:1,
+        review: 'i will come back again',
+        stars: 2
+      },
+      {
+        spotId: 12,
+        userId: 2,
+        review: 'not a bad place',
+        stars: 2
+      },{
+        spotId: 12,
         userId: 3,
         review: 'this is the best place ever',
         stars: 5
