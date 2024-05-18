2b2639177ea77d2a76690489137501531dfe94af
Mengxuan Liang <mengxuanliang007@gmail.com>
2024-05-17 20:14:28 -0400

    add a S for Booking route

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -44,7 +44,7 @@ router.get('/current', requireAuth, async(req,res)=> {
         }
     });
     await previewImage(bookings)
-    res.json({Booking: bookings})
+    res.json({Bookings: bookings})
 })
 
 
